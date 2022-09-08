2db70007e08da623a29960341ca4d626083a215f
Barretim <112975463+Barretim@users.noreply.github.com>
2022-09-08 17:22:56 -0400

    Create INSERT MANY.sql

---
+++ SQL/Encontro_3/INSERT MANY.sql
@@ -0,0 +1,10 @@
+INSERT INTO Cliente(Nome,Documento,Telefone,Email,Endereço)
+VALUES 
+('Lucas','456','987654321','Lucas@teste.com','Rua x, n.11'),
+('Joao','789','987836391','joao@teste.com','Rua x, n.12'),
+('Pedro','343','339393291','Pedro@teste.com','Rua x, n.13'),
+('Tiago','293','537337421','Tiago@teste.com','Rua x, n.14'),
+('Judas','666','984823321','judas@teste.com','Rua x, n.15'),
+('Andre','936','982354321','ANDRE@teste.com','Rua x, n.16'),
+('Marcus','233','798676321','MARCUS@teste.com','Rua x, n.17')
+GO
