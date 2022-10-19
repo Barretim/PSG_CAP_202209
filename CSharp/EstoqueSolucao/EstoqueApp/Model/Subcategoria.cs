e8c794bce5563a7883a671734f5dc7125252b4cb
Barretim <bruno.barreto20013@gmail.com>
2022-10-19 17:34:11 -0400

    Encontro_29
    Aprendendo sobre POO

---
+++ CSharp/EstoqueSolucao/EstoqueApp/Model/Subcategoria.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstoqueApp.Model
+{
+    public class Subcategoria : BaseEstoque
+    {
+
+        private int codigoCategoria;
+
+        public int CodigoCategoria { get => codigoCategoria; set => codigoCategoria = value; }
+
+        public Subcategoria() : base()
+        {
+        }
+
+        public Subcategoria(int codigo, string descricao, bool ativo, DateTime dataInclusao, int codigoCategoria)
+            : base(codigo, descricao, ativo, dataInclusao)
+        {
+            this.codigoCategoria = codigoCategoria;
+        }
+
+
+    }
+}
