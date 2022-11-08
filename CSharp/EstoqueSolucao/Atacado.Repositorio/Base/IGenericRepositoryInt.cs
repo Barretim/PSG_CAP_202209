f8fad37e399e23326cb598161d7adc702bf046a9
Barretim <bruno.barreto20013@gmail.com>
2022-11-08 17:24:46 -0400

    Encontro_42
    Criando GenericRepositorys em Repositorio e ajustando em Servico

---
+++ CSharp/EstoqueSolucao/Atacado.Repositorio/Base/IGenericRepositoryInt.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atacado.Repositorio.Base
+{
+    public interface IGenericRepositoryInt<T> : IGenericRepository<T> where T : class
+    {
+        T GetById(int id);
+
+        T Delete(int id);
+    }
+}
